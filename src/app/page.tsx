b9124d257dfa633802749620fc6222ded923ad9a
Robert Crocker <rcrocker13@gmail.com>
2024-06-26 10:06:01 -0500

    'Add usehooks to dependencies, import and use in components. Enhances functionality and modularity.'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import icon_list from "@/../public/images/icon-list.svg";
 import icon_success from "@/../public/images/icon-success.svg";
 import illustration_desktop from "@/../public/images/illustration-sign-up-desktop.svg";
+import illustration_mobile from "@/../public/images/illustration-sign-up-mobile.svg";
 import Image from "next/image";
 import {
   Card,
@@ -15,11 +16,16 @@ import {
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-
-import { Attribution } from "@/components/fem-attribution";
+import { useWindowSize } from "@uidotdev/usehooks";
 
 export default function Home() {
   const [isVisible, setIsVisible] = useState(true);
+
+  const { width } = useWindowSize();
+  if (!width) return null;
+  const mobile = width < 768;
+
+  const illustration = mobile ? illustration_mobile : illustration_desktop;
 
   const handleSubmit = () => {
     setIsVisible(false);
@@ -29,14 +35,14 @@ export default function Home() {
     setIsVisible(true);
   };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-2 p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-0 bg-gray-700 md:p-24">
       {/* card */}
       {/* <!-- Sign-up form start --> */}
       {isVisible && (
-        <Card className="flex h-[641px] w-[928px] items-center gap-6 rounded-[36px] p-6">
-          <section className="p-9">
-            <CardHeader className="mb-6 flex flex-col gap-6 p-0">
-              <CardTitle className="text-5xl font-bold">
+        <Card className="flex w-full flex-col-reverse items-center gap-0 rounded-none p-0 md:h-[641px] md:w-[928px] md:flex-row md:gap-6 md:rounded-[36px] md:p-6">
+          <section className="p-6 md:p-9">
+            <CardHeader className="mb-6 flex flex-col gap-2 p-0 md:gap-6">
+              <CardTitle className="text-[40px] font-bold md:text-5xl">
                 Stay updated!
               </CardTitle>
               <CardDescription className="text-base">
@@ -45,15 +51,15 @@ export default function Home() {
             </CardHeader>
             <CardContent className="flex flex-col gap-10 p-0">
               <ul className="flex flex-col gap-[10px]">
-                <li className="flex items-center gap-4">
+                <li className="flex items-start gap-4">
                   <Image src={icon_list} alt={"list item icon"} />
                   Product discovery and building what matters
                 </li>
-                <li className="flex items-center gap-4">
+                <li className="flex items-start gap-4">
                   <Image src={icon_list} alt={"list item icon"} />
                   Measuring to ensure updates are a success
                 </li>
-                <li className="flex items-center gap-4">
+                <li className="flex items-start gap-4">
                   <Image src={icon_list} alt={"list item icon"} />
                   And much more!
                 </li>
@@ -74,7 +80,7 @@ export default function Home() {
               </form>
             </CardContent>
           </section>
-          <Image src={illustration_desktop} alt={"Sign-up Illustration"} />
+          <Image className="" src={illustration} alt={"Sign-up Illustration"} />
         </Card>
       )}
       {!isVisible && (
@@ -97,11 +103,6 @@ export default function Home() {
           </CardContent>
         </Card>
       )}
-
-      <Attribution
-        name={"Robert Crocker"}
-        link={"https://www.frontendmentor.io/profile/robcrock"}
-      />
     </main>
   );
 }
